b628523171cbe6d1ee06578c94fb16e504b708bb
Yuri-RC <cttyuriresende@gmail.com>
2022-02-02 17:22:15 -0300

    exercicios 4.1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
@@ -0,0 +1,74 @@
+// Exercício 01
+// const a = 5;
+// const b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Exercício 02
+// const num1 = 10;
+// const num2 = 15;
+
+// if (num1 > num2) {
+//   console.log("O maior número é: " + num1);
+// } else {
+//   console.log("O maior número é: " + num2);
+// }
+
+// Exercício 03
+// const a = 9;
+// const b = 1;
+// const c = 4;
+
+// if (a > b && a > c) {
+//   console.log(a + " é o maior número");
+// } else if (c > b) {
+//   console.log(c + " é o maior número");
+// } else {
+//   console.log(b + " é o maior número");
+// }
+
+// Exercício 04
+// const posNeg = -1;
+
+// if (posNeg > 0) {
+//   console.log("Positive");
+// }
+// else if (posNeg < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// }
+
+// Exercício 05
+// const ang1 = 50;
+// const ang2 = 50;
+// const ang3 = 80;
+
+// if ((ang1 + ang2 + ang3) == 180) {
+//   console.log("É um triângulo");
+// } else {
+//   console.log("Não é um triângulo");
+// }
+
+// Exercício 07
+// const porcentagem = 76;
+
+// if (porcentagem < 0 || porcentagem > 100) {
+//   console.log("Valor incorreto");
+// } else if (porcentagem >= 90) {
+//   console.log("A");
+// } else if (porcentagem >= 80) {
+//   console.log("B");
+// } else if (porcentagem >= 70) {
+//   console.log("C");
+// } else if (porcentagem >= 60) {
+//   console.log("D");
+// } else if (porcentagem >= 50) {
+//   console.log("E");
+// } else {
+//   console.log("F");
+// }

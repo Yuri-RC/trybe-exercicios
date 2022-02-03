9b145760478431df69e9ca20782856f8d31bd970
Yuri-RC <cttyuriresende@gmail.com>
2022-02-03 16:33:19 -0300

    ex dia 4.2

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script.js
@@ -0,0 +1,73 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let cont = 0
+
+// Ex 01
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
+// Ex 02
+// for (let number of numbers) {
+//   cont += number
+// }
+// console.log(cont);
+
+//Ex 03
+// for (let number of numbers) {
+//   cont += number
+// }
+// console.log(cont/numbers.length);
+
+//Ex 04
+// for (let number of numbers) {
+//   cont += number
+// }
+// let media = cont/numbers.length
+// if (media > 20) {
+//   console.log("Maior que 20");
+// } else {
+//   console.log("Menor que 20");
+// }
+
+//Ex 05
+// let maiorNumero = numbers[0]
+// for (let number of numbers) {
+//   if (maiorNumero < number) {
+//     maiorNumero = number;
+//   }
+// }
+// console.log(maiorNumero);
+
+//Ex 06
+// for (let number of numbers) {
+//   if (number % 2 == 1) {
+//     cont ++
+//   }
+// }
+
+// if (cont != 0) {
+//   console.log(cont + " números ímpares");
+// } else {
+//   console.log("nenhum valor ímpar encontrado");
+// }
+
+//Ex 07
+// let menorNumero = numbers[0]
+// for (let number of numbers) {
+//   if (menorNumero > number) {
+//     menorNumero = number;
+//   }
+// }
+// console.log(menorNumero);
+
+//Ex 08
+// let lista = []
+// for (let i = 1; i <= 25; i++) {
+//   lista.push(i)
+// }
+// console.log(lista);
+
+// // Ex 09
+// for (num of lista) {
+//   console.log(num/2);
+// }
